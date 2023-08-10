317c8618017bb1402a7040a30460b9bbfea02a35
Genís Martínez <genismartinez@MacBook-Pro-de-Genis.local>
2023-08-10 09:46:44 +0200

    First Working Version

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+
+namespace MyWebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public string VideoUrl { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!string.IsNullOrEmpty(VideoUrl))
+            {
+                var youtube = new YoutubeClient();
+                var video = await youtube.Videos.GetAsync(VideoUrl);
+
+                if (video != null)
+                {
+                    var streamManifest = await youtube.Videos.Streams.GetManifestAsync(video.Id);
+                    var streamInfo = streamManifest.GetMuxedStreams().GetWithHighestVideoQuality();
+
+                    if (streamInfo != null)
+                    {
+                        using var httpClient = new HttpClient();
+                        var videoBytes = await httpClient.GetByteArrayAsync(streamInfo.Url);
+
+                        if (videoBytes != null && videoBytes.Length > 0)
+                        {
+                            return File(videoBytes, "video/mp4", $"{video.Id}.mp4");
+                        }
+                    }
+                }
+            }
+
+            // Si no se puede procesar el URL, redirigir al usuario de vuelta a la página
+            return RedirectToPage();
+        }
+    }
+}
